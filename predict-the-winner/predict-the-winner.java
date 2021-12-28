fe1066c62c7035cec212fade9c8766e6b363500f
DIvyanshSC <80683137+DIvyanshSC@users.noreply.github.com>
2021-12-28 22:01:24 +0530

    Time: 67 ms (18.70%), Space: 36.3 MB (79.79%) - LeetHub

---
+++ predict-the-winner/predict-the-winner.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int play1 = 0, play2 = 0;
+    public boolean PredictTheWinner(int[] nums) {
+        return winner(nums, 0, nums.length - 1, 1) >= 0;
+    }
+    
+    public int winner(int[] nums, int s, int e, int turn) {
+        if (s == e)
+            return turn * nums[s];
+        int a = turn * nums[s] + winner(nums, s + 1, e, -turn);
+        int b = turn * nums[e] + winner(nums, s, e - 1, -turn);
+        
+        return turn * Math.max(turn * a, turn * b);
+    }
+}
