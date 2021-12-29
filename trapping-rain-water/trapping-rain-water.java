7a1500ecdc0159da953d98316214136d1ee15480
Divyansh <80683137+DivyanshSC@users.noreply.github.com>
2021-12-30 01:34:34 +0530

    Time: 8 ms (9.09%), Space: 42.4 MB (6.94%) - LeetHub

---
+++ trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int trap(int[] height) {
+        int ans = 0;
+        int curr = 0;
+        
+        Stack<Integer> st = new Stack<>();
+        
+            
+        while(curr < height.length) {
+            while(!st.isEmpty() && height[curr] > height[st.peek()]) {
+                int top = st.peek();
+                st.pop();
+                if(st.isEmpty()) {
+                    break;
+                }
+                
+                int dist = curr - st.peek() - 1;
+                int area = Math.min(height[curr], height[st.peek()]) - height[top];
+                ans += dist * area;
+            }
+            
+            st.push(curr++);
+        }
+        
+        return ans;
+    }
+}
