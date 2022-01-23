ad9977831b8a1c2343209caac7d95502073ff7cc
Divyansh <80683137+div-devs@users.noreply.github.com>
2022-01-23 17:52:37 +0530

    Time: 0 ms (100.00%), Space: 38.3 MB (22.83%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -1,25 +1,25 @@
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
         
-        ArrayList<Integer> arr = new ArrayList<>();
-        
-        for(int i=1; i<=9; i++) {
-            dfs(low, high, i, 0, arr);
-        }
-        
-        arr.sort((a,b) -> a - b);
-        return arr;
+        String str = "123456789";
+
+		int count1 = Integer.toString(low).length();
+		int count2 = Integer.toString(high).length();
+
+		List<Integer> lst = new ArrayList<>();
+		while(count1 <= count2){
+			for(int j=0; j<9-count1+1; j++){
+				int num = Integer.parseInt(str.substring(j, j+count1));
+				if(num >= low && num <= high){
+					lst.add(num);
+				}
+			}
+			count1++;
+		}
+
+
+		return lst;
     }
     
-    void dfs(int low, int high, int i, int num, ArrayList<Integer> ans) {
-        
-        if (num >= low && num <= high) {
-            ans.add(num);
-        }
-        // case 2: [1000, 13000] if i=10 and num is 6789, then we should stop
-        if (num > high || i > 9) { 
-            return;
-        }
-        dfs(low, high, i+1, num*10 + i, ans);
-    }
+
 }
