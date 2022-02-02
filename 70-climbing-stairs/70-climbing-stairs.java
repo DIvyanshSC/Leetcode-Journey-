5e788a54baaa05f03b01fa0f86a43b53bea2c529
Divyansh <80683137+div-devs@users.noreply.github.com>
2022-02-02 16:18:00 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (5.72%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -4,16 +4,30 @@ class Solution {
     }
     
     private int helper(int n, int[] dp) {
-        if(n == 1 || n == 2) {
-            return n;
+        if(n == 0 || n == 1) {
+            return 1;
         }
-        dp[1] = 1;
-        dp[2] = 2;
         
-        for(int i = 3; i <= n; i++) {
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++) {
             dp[i] = dp[i-1] + dp[i-2];
         }
         
         return dp[n];
+        
+//         if(n == 1 || n == 2) {
+//             return n;
+//         }
+        
+//         dp[1] = 1;
+//         dp[2] = 2;
+        
+//         for(int i = 3; i <= n; i++) {
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+        
+//         return dp[n];
     }
 }
