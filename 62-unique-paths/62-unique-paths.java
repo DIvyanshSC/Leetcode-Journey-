b8ad2d04adf0ca880962a924e9ad949b02b2eb4c
Divyansh <80683137+div-devs@users.noreply.github.com>
2022-02-11 22:12:21 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (6.06%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,13 +1,46 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                dp[i][j] = -1;
-            }
+        
+//         int[][] dp = new int[m][n];
+//         for(int i = 0; i < m; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 dp[i][j] = -1;
+//             }
+//         }
+
+//         return f(m-1, n-1, dp);
+        
+        return func_lessSpace(m-1, n-1);
+    }
+    
+    private static int func_lessSpace(int row, int col) {
+
+        // declaring space optimsed 1d dp
+        int[] prev = new int[col+1];
+        for(int i = 0; i < col; i++) {
+            prev[i] = -1;
         }
 
-        return f(m-1, n-1, dp);
+
+        for(int r = 0; r <= row; r++) {
+            int[] temp = new int[col+1];
+            for(int c = 0; c <= col; c++) {
+
+                // base case
+                if(r == 0 || c == 0){
+                    temp[c] = 1;
+
+                    continue;
+                }
+
+                if(c > 0) temp[c] = temp[c-1]+prev[c];
+
+            }
+
+            prev = temp;
+        }
+
+        return prev[col];
     }
     
     private static int f(int row, int col, int[][] dp) {
