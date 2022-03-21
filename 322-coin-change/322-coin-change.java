ad4598c229edb534637732912da0c65218eb71da
Divyansh <80683137+div-devs@users.noreply.github.com>
2022-03-21 22:23:57 +0530

    Time: 31 ms (44.09%), Space: 55.5 MB (16.34%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,35 +1,40 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[coins.length][amount+1];
-        for(int[] i : dp) {
-            Arrays.fill(i, -1);
-        }
-
-        int ans = f(coins.length-1, amount, coins, dp);
+        int ans = space_optimised(coins, amount, coins.length);
         return ans == (int)1e9 ? -1 : ans;
     }
 
-    private static int f(int ind, int T, int[] coins,int[][] dp) {
+    private static int space_optimised(int[] coins, int T, int n) {
+        // TC: O(n*T) ; SC: O(T)
+
+        //array 1D creation
+        int[] prev = new int[T+1];
+
         //base case
-        if(ind == 0) {
-            if(T % coins[ind] == 0) {
-                return T/coins[ind];
+        for(int t = 0; t <= T; t++) {
+            if(t % coins[0] == 0) {
+                prev[t] = t/coins[0];
             }
 
-            return (int)1e9;
+            else prev[t] = (int)1e9;
         }
 
-        if(dp[ind][T] != -1) return dp[ind][T];
+        //recursive case
+        for(int i = 1; i < n; i++) {
+            int[] curr = new int[T+1];
+            for(int t = 0; t <= T; t++) {
+                int notTake = prev[t];
+                int take = Integer.MAX_VALUE;
 
-        //recursive calls
-        int notTake = f(ind-1, T, coins, dp);
+                if(coins[i] <= t) {
+                    take = 1 + curr[t-coins[i]];
+                }
 
-        int take = Integer.MAX_VALUE;
-        if(coins[ind] <= T) {
-            take = 1 + f(ind, T - coins[ind], coins, dp);
+                curr[t] = Math.min(take, notTake);
+            }
+            prev = curr;
         }
 
-        dp[ind][T] = Math.min(take, notTake);
-        return dp[ind][T];
+        return prev[T];
     }
 }
