7bbe8d228039336e61a22580e3cf0946a8f15cf7
Divyansh <80683137+div-devs@users.noreply.github.com>
2022-03-25 12:28:28 +0530

    Time: 1 ms (99.54%), Space: 40.6 MB (79.82%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        // sort on basis of absolute diff of cities
+		Arrays.sort(costs, (a1, a2) -> a1[0] - a1[1] - a2[0] + a2[1]);
+		// hold answer
+		int cost = 0;
+		// length of array. i.e. n
+		int n = costs.length;
+		// iterate half time
+		for (int i = 0; i < n / 2; i++) {
+			// add 0th index element of first half and 1st index element of second half
+			cost += costs[i][0] + costs[i + n / 2][1];
+		}
+		// return answer
+		return cost;
+    }
+}
