2d16a873e1819370f1182be5f6a6762e046202b3
Divyansh <80683137+div-devs@users.noreply.github.com>
2022-03-30 12:21:09 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (45.83%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int[] ans = search(matrix, target);
+        if(ans[0] == -1 && ans[1] == -1) {
+            return false;
+        } 
+        
+        else {
+            return true;
+        }
+    }
+    
+    public int[] search(int[][] matrix, int target) {
+        int r = 0;
+        int c;
+        if(matrix.length >= 1) {
+            c = matrix[0].length - 1;
+        }
+        else {
+            c = matrix.length;
+        }
+        
+        
+        while(r < matrix.length && c >= 0) {
+            
+            if(matrix[r][c] == target) {
+                return new int[] {r, c};
+            }
+            
+            else if(matrix[r][c] < target) {
+                r++;
+            }
+            
+            // if(matrix[r][c] > target) {
+            //     c--;
+            // }
+            else {
+                c--;
+            }
+            
+        }
+        
+        return new int[] {-1, -1};
+    }
+}
