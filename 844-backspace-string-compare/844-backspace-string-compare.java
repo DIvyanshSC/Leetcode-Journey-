7e24a20098c3bbe83188f15b586d57c6ab08038b
Divyansh <80683137+div-devs@users.noreply.github.com>
2022-05-01 13:37:28 +0530

    Time: 1 ms (87.56%), Space: 41.5 MB (73.19%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,11 +1,41 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        String s1 = stackUse(s);
-        String s2 = stackUse(t);
+        /**
+            Time Complexity: O(M + N)
+            Space Complexity: O(1)
+        */
         
-        if(s1.equals(s2)) return true;
+        int i = s.length() - 1, j = t.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) { // While there may be chars in build(S) or build (T)
+            while (i >= 0) { // Find position of next possible char in build(S)
+                if (s.charAt(i) == '#') {skipS++; i--;}
+                else if (skipS > 0) {skipS--; i--;}
+                else break;
+            }
+            while (j >= 0) { // Find position of next possible char in build(T)
+                if (t.charAt(j) == '#') {skipT++; j--;}
+                else if (skipT > 0) {skipT--; j--;}
+                else break;
+            }
+            // If two actual characters are different
+            if (i >= 0 && j >= 0 && s.charAt(i) != t.charAt(j))
+                return false;
+            // If expecting to compare char vs nothing
+            if ((i >= 0) != (j >= 0))
+                return false;
+            i--; j--;
+        }
         
-        else return false;
+        return true;
+        
+//         String s1 = stackUse(s);
+//         String s2 = stackUse(t);
+        
+//         if(s1.equals(s2)) return true;
+        
+//         else return false;
     }
     
     private static String stackUse(String s) {
