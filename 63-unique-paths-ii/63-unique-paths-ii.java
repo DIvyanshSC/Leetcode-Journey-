47815ed2bb36f89b6f9d0efb302d0a2e41de28ae
Divyansh <80683137+div-devs@users.noreply.github.com>
2022-05-20 15:27:01 +0530

    Time: 1 ms (52.25%), Space: 42.7 MB (23.61%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -11,17 +11,86 @@ class Solution {
             }
         }
         
-        int ans = pathCount(obstacleGrid, m-1, n-1, dp);
-        System.out.println(m-1);
-        System.out.println(n-1);
-        System.out.println();
-
-        for(int[] arr : dp) {
-            System.out.println(Arrays.toString(arr));
+        // int ans = pathCount(obstacleGrid, m-1, n-1, dp);
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                dp[i][j] = 0;
+            }
         }
-        System.out.println();
-
+        int ans = tabulated(obstacleGrid, dp, m, n);
         return ans;
+        // return spaceOptimised(m, n, obstacleGrid);
+    }
+    
+    private static int spaceOptimised(int n, int m, int[][] maze) {
+        // Write your code here.
+        int prev[] = new int[n];
+
+        for(int i=0; i<n; i++) {
+
+            int temp[] = new int[m];
+
+            for(int j=0; j<m; j++) {
+
+                if(i>0 && j>0 && maze[i][j] == 1) {
+                    temp[j]=0;
+                    continue;
+                }
+
+                if(i==0 && j==0){
+                    temp[j]=1;
+                    continue;
+                }
+
+                int up=0;
+                int left =0;
+
+                if(i>0) up = prev[j];
+
+                if(j>0) left = temp[j-1];
+
+                temp[j] = up + left;
+            }
+            System.out.println(Arrays.toString(prev));
+            prev = temp;
+
+        }
+
+        return prev[n-1];
+    }
+    
+    private static int tabulated(int[][] maze, int[][] dp, int m, int n) {
+        
+        for(int r = 0; r < m; r++) {
+            for(int c = 0; c < n; c++) {
+
+                // base case
+                if(maze[r][c] == 1){
+                    dp[r][c] = 0;
+                    continue;
+                }
+                if(r==0 && c==0){
+                    dp[r][c] = 1;
+                    continue;
+                }
+
+                // recursive case
+                int down = 0;
+                int right = 0;
+
+                if(r > 0) {
+                    down = dp[r - 1][c];
+                }
+
+                if(c > 0) {
+                    right = dp[r][c - 1];
+                }
+
+                dp[r][c] = down + right;
+            }
+        }
+
+        return dp[m-1][n-1];
     }
     
     private static int pathCount(int[][] maze, int r, int c, int[][] dp) {
